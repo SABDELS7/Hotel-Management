9378da05c1177ea3d8ab1490b2c15d5c47faa66d
SABDELS7 <salmiabdderrahmane@gmail.com>
2025-03-25 04:36:07 +0000

    first commit

---
+++ backend/src/main/java/com/example/hotelmanagementbackend/exception/OurException.java
@@ -0,0 +1,8 @@
+package com.example.hotelmanagementbackend.exception;
+
+public class OurException extends RuntimeException {
+
+    public OurException(String message) {
+        super(message);
+    }
+}
